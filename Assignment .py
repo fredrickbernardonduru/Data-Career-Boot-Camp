ceac805649dc47c3c09b74364c667f105df79cab
Fredrich Bernard Onduru <113997263+fredrickbernardonduru@users.noreply.github.com>
2024-05-05 23:18:21 +0300

    Add files via upload

---
+++ Assignment .py
@@ -0,0 +1,11 @@
+def analyze_numbers(manamba,sum):
+    manamba = [20,48,7,84,93,74]
+    sum = manamba[0] + manamba[1]+ manamba[2]+ manamba[3]+ manamba[4]+ manamba[5]
+    if sum/6> 50:
+        print("Above avarage")
+    elif sum == 50:
+        print("Average")
+    elif sum < 50:
+        print("Below avarage")
+
+analyze_numbers(None,None)
